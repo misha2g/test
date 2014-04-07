47036bb966249f837c41ecfb2b5c6a7620a4609c
Misha <misha2g@m2g.local>
2014-04-07 13:19:54 +0300

    misca

--- Test/M2GAppDelegate.h
+++ Test/M2GAppDelegate.h
@@ -11,10 +11,8 @@
 @interface M2GAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property (weak) NSMutableString *string;
 @property (nonatomic) NSString* property;
 
-
-@property (weak) NSMutableString *string;
 
 @end
